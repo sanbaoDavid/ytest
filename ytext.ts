67678114c968e066833b90fe7ee3cdf17246e429
yangdonghe <yangdonghe@vipkid.com.cn>
2019-11-07 00:28:15 +0800

    更新： 1.把下划线预制体上传了，虽然谁都能做。。 2.增加字体 3.增加字体颜色功能 4.增加了debug模式，目前只是在有标签的地方会显示文字的边框，激活和未激活的位置，方便调整 5.优化位置排版 6.增加了渲染结束后的回调列表

--- ytext.ts
+++ ytext.ts
@@ -21,10 +21,6 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Page22ytext extends cc.Component {
-  //下划线预制体
-  @property(cc.Prefab)
-  line: cc.Prefab = null;
-
   //要渲染的文本
   @property
   string: string = "";
@@ -37,6 +33,22 @@ export default class Page22ytext extends cc.Component {
   @property
   lineSpacing: number = 20;
 
+  //下划线预制体
+  @property(cc.Prefab)
+  line: cc.Prefab = null;
+
+  //字体
+  @property(cc.Font)
+  font: cc.Font = null;
+
+  //字体颜色
+  @property(cc.Color)
+  fontColor: cc.Color = cc.Color.BLACK;
+
+  //debug
+  @property(Boolean)
+  debug: Boolean = false;
+
   //单双标签列表
   _singleFlag: Array<string> = ["br"];
   _doubleFlag: Array<string> = ["u"];
@@ -47,12 +59,15 @@ export default class Page22ytext extends cc.Component {
       this._pointer.y -= this.fontSize + this.lineSpacing;
     },
     u: node => {
-      let ratio = 5;
+      let ratio = 2.63;
       let c = node.getChildByName(this.line.name);
       if (c === null) {
         c = cc.instantiate(this.line);
         c.parent = node;
-        c.height = this.fontSize * 0.1;
+        c.height = this.fontSize * 0.08;
+        let color = cc.Color.BLACK;
+        color.fromHEX("#29333E"); 
+        c.color = color;
         c.width = Math.max(this.fontSize * ratio, node.width);
         c.y = -this.fontSize * 0.739;
       }
@@ -61,16 +76,27 @@ export default class Page22ytext extends cc.Component {
         node.x += Math.abs((this.fontSize * ratio) / 2 - node.width / 2);
         this._pointer.x += this.fontSize * ratio - node.width;
       }
+      let color = cc.Color.BLACK;
+      color.fromHEX("#ff6600"); 
+      node.color = color
     },
   };
 
-  //以下标签不可改
+  //以下变量不可改
   //label列表 根据输入文本动态创建
   _labList: Array<cc.Node> = [];
   //实际组件解析文本时所用的字符串变量
   _config: string = "";
   //位置指示
   _pointer: cc.Vec2;
+  //渲染结束回调
+  _renderedLsit: Array<any> = [];
+  //debug画线辅助
+  _brush: cc.Node = undefined;
+
+  onRendered(func) {
+    this._renderedLsit.push(func);
+  }
 
   parseStr(str: string = undefined) {
     this._config = str ? str : this.string;
@@ -218,17 +244,21 @@ export default class Page22ytext extends cc.Component {
           el.active = true;
           let l = el.getComponent(cc.Label);
           l.string = "";
+          l.font = this.font
           l.fontSize = this.fontSize;
           l.lineHeight = this.fontSize;
           el.height = this.fontSize;
+          el.color = this.fontColor;
           return el;
         }
       }
       let node = new cc.Node();
       node.parent = this.node;
       node.name = "lab" + this._labList.length + 1;
+      node.color = this.fontColor;
       let l = node.addComponent(cc.Label);
       l.string = "";
+      l.font = this.font
       l.fontSize = this.fontSize;
       l.lineHeight = this.fontSize;
       node.height = this.fontSize;
@@ -246,24 +276,79 @@ export default class Page22ytext extends cc.Component {
       });
     };
     let updateLayout = () => {
-      let minx = 0;
-      let miny = 0;
-      let maxx = 0;
-      let maxy = 0;
+      let { minx, maxy, width, height } = this.labFactory().getRange();
+      let offsetX = -width / 2 - minx;
+      let offsetY = height / 2 - maxy;
       this._labList.forEach(el => {
-        maxx = Math.max(el.x + el.width / 2, maxx);
-        maxy = Math.max(el.y, maxy);
-        minx = Math.min(el.x - el.width / 2, minx);
-        miny = Math.min(el.y, miny);
-      });
-      let offsetX = (maxx - minx) / 2;
-      let offsetY = Math.max((maxy - miny) / 2, this.fontSize);
-      this._labList.forEach(el => {
-        el.x -= offsetX;
+        el.x += offsetX;
         el.y += offsetY;
       });
     };
-    return { getLab, recovery, updateLayout };
+    let getRange = () => {
+      let aMinx = []
+      let aMaxx = []
+      let aMiny = []
+      let aMaxy = []
+      this._labList.forEach(el=>{
+        if(el.active){
+          aMinx.push(el.x - el.width / 2)
+          aMaxx.push(el.x + el.width / 2)
+          aMiny.push(el.y - el.height / 2)
+          aMaxy.push(el.y + el.height / 2)
+        }
+      })
+      let minx = Math.min.apply(Math, aMinx);
+      let miny = Math.min.apply(Math, aMiny);
+      let maxx = Math.max.apply(Math, aMaxx);
+      let maxy = Math.max.apply(Math, aMaxy);
+      let width = maxx - minx;
+      let height = maxy - miny;
+      return { minx, miny, maxx, maxy, width, height };
+    };
+    let getPen = () => {
+      if (!this._brush) {
+        this._brush = new cc.Node();
+        this._brush.parent = this.node;
+        this._brush.addComponent(cc.Graphics).lineWidth = 1;
+      }
+      return this._brush.getComponent(cc.Graphics);
+    };
+    let drawClear = () => {
+      const pen = this.labFactory().getPen();
+      pen.clear();
+    };
+    let drawRim = () => {
+      const pen = this.labFactory().getPen();
+      this._labList.forEach(el => {
+        pen.strokeColor = cc.Color.GREEN;
+        if(!el.active){
+          console.log('ydh-drawRim-activeFalse');
+          
+          pen.strokeColor = cc.Color.BLUE;
+        }
+        //到左下
+        pen.moveTo(el.x - el.width / 2, el.y - el.height / 2);
+        //去左上
+        pen.lineTo(el.x - el.width / 2, el.y + el.height / 2);
+        //去右上
+        pen.lineTo(el.x + el.width / 2, el.y + el.height / 2);
+        //去右下
+        pen.lineTo(el.x + el.width / 2, el.y - el.height / 2);
+        //回到左下
+        pen.lineTo(el.x - el.width / 2, el.y - el.height / 2);
+        //绘制
+        pen.stroke();
+      });
+    };
+    return {
+      getLab,
+      recovery,
+      updateLayout,
+      getRange,
+      drawRim,
+      drawClear,
+      getPen,
+    };
   }
 
   noMarkRender(content: string) {
@@ -277,6 +362,9 @@ export default class Page22ytext extends cc.Component {
 
   labRender(config) {
     this.labFactory().recovery();
+    if (this.debug) {
+      this.labFactory().drawClear();
+    }
     this._pointer = cc.v2(0, 0);
     for (let index = 0; index < config.length; index++) {
       const el = config[index];
@@ -307,5 +395,11 @@ export default class Page22ytext extends cc.Component {
       this._pointer.x += n.width;
     }
     this.labFactory().updateLayout();
+    this._renderedLsit.forEach(el => {
+      el(this.labFactory().getRange());
+    });
+
+    //debug
+    if (this.debug) this.labFactory().drawRim();
   }
 }
